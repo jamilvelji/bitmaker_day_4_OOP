4605bdb9a43e697e972a4b59776c8e4e28ed1378
Jamil Velji <jamil@jamilvelji.com>
2014-08-12 14:38:13 -0400

    Mars Rover problem skeleton added to rover.rb

---
+++ rover.rb
@@ -0,0 +1,78 @@
+class Plateau
+	def initialize(x_grid_limit, y_grid_limit)
+		@x_grid_limit = x_grid_limit.to_i
+		@y_grid_limit = y_grid_limit.to_i
+	end
+end
+
+class Rover < Plateau
+	def initialize(x_position, y_position, direction)
+		@x_position = x_position.to_i
+		@y_position = y_position.to_i
+		@direction = direction.upcase
+	end
+
+	def in_grid
+		if @x_position >= x_grid_limit
+			puts "Cannot move any further along the x-axis"
+		elsif @y_position >= y_grid_limit
+			puts "cannoy move any further along the y-axis"
+		end
+	end
+
+	def rover_input(movements)
+		separated_movements = movements.split(//)
+		separated_movements.each do |movement|
+			case movement
+				when "L"
+					turn_left
+				when "R"
+					turn_right
+				when "M"
+					move_one
+				else
+					puts "Input not recognized"
+			end
+		end
+	end
+
+	def turn_left
+		case @direction
+			when "N" then "E"
+			when "E" then "S"
+			when "S" then "W"
+			when "W" then "N"
+		end
+	end
+
+	def turn_right
+		case @direction
+			when "N" then "W"
+			when "W" then "S"
+			when "S" then "E"
+			when "E" then "N"
+		end
+	end
+
+	def move_one
+		case @direction
+			when "N"
+				@y_position += 1
+			when "E"
+				@x_position += 1
+			when "W"
+				@x_position -= 1
+			when "S"
+				@y_position -= 1
+		end
+	end
+	puts "#{@x_position}, #{@y_position}, #{@direction}"
+end
+
+plateau = Plateau.new(5, 5)
+
+rover1 = Rover.new(1, 2, "N")
+rover1.rover_input("LMLMLMLMM")
+
+rover2 = Rover.new(3, 3, "E")
+rover2.rover_input("MMRMMRMRRM")
