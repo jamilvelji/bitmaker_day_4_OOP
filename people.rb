436706ddb9d341edb5f793bfc000dd8b71fa08c4
Jamil Velji <jamil@jamilvelji.com>
2014-08-12 12:37:57 -0400

    Called learn and teach methods

--- people.rb
+++ people.rb
@@ -24,5 +24,13 @@ end
 
 instructor = Instructor.new("Chris")
 instructor.greeting
+
 student = Student.new("Cristina")
 student.greeting
+
+instructor.teach
+student.learn
+
+#student.teach
+#This does not work because teach is a method within the
+#Instructor class and is not accessable by another class
